945063926f030ca9d1b4fb475d10b021162a604c
david1707 <DavidAlacant@gmail.com>
2019-08-31 18:31:27 +0200

    Second lesson done

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 A Scrapy bot that I'm using to scrape 'http://books.toscrape.com/' URL for my tutorial on http://letslearnabout.net/
 
 
-## First lesson - Your first Scrapy spider | Creating a scrapy spider, fetching and saving the data
-- Creating an environment and installing everything
-- Creating a project
-- Creating a spider
-- A small trick: Scrapy shell
-- How Xpath works and how we fetch the data
-- Manipulating data and yielding the information
-- Running the spider
-- Saving the data as a .json, .csv and .xml files
+## First lesson - Your first Scrapy spider | Scraping items in their detail page
+- Use callbacks to other Scrapy class functions
+- How to extract routes with relative images
+- How to select elements by tab, class, partial class
--- books/books/spiders/spider.py
+++ books/books/spiders/spider.py
@@ -6,18 +6,52 @@ class SpiderSpider(scrapy.Spider):
     name = 'spider'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
+    base_url = 'http://books.toscrape.com/'
 
     def parse(self, response):
         all_books = response.xpath('//article[@class="product_pod"]')
-        
+
         for book in all_books:
-            title = book.xpath('.//h3/a/@title').extract_first()
-            price = book.xpath('.//div/p[@class="price_color"]/text()').extract_first()
-            image_url = self.start_urls[0] + book.xpath('.//img[@class="thumbnail"]/@src').extract_first()
-            book_url = self.start_urls[0] + book.xpath('.//h3/a/@href').extract_first()
-            yield {
-                'title': title,
-                'price': price,
-                'Image URL': image_url,
-                'Book URL': book_url,
-            }
+            book_url = self.start_urls[0] + \
+                book.xpath('.//h3/a/@href').extract_first()
+
+            yield scrapy.Request(book_url, callback=self.parse_book)
+
+    def parse_book(self, response):
+        title = response.xpath('//div/h1/text()').extract_first()
+
+        relative_image = response.xpath('//div[@class="item active"]/img/@src').extract_first()
+        final_image = self.base_url + relative_image.replace('../..', '')
+
+        price = response.xpath(
+            '//div[contains(@class, "product_main")]/p[@class="price_color"]/text()').extract_first()
+        stock = response.xpath(
+            '//div[contains(@class, "product_main")]/p[contains(@class, "instock")]/text()').extract()[1].strip()
+        stars = response.xpath(
+            '//div/p[contains(@class, "star-rating")]/@class').extract_first().replace('star-rating ', '')
+        description = response.xpath(
+            '//div[@id="product_description"]/following-sibling::p/text()').extract_first()
+        upc = response.xpath(
+            '//table[@class="table table-striped"]/tr[1]/td/text()').extract_first()
+        price_excl_tax = response.xpath(
+            '//table[@class="table table-striped"]/tr[3]/td/text()').extract_first()
+        price_inc_tax = response.xpath(
+            '//table[@class="table table-striped"]/tr[4]/td/text()').extract_first()
+        tax = response.xpath(
+            '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first()
+        number_of_reviews = response.xpath(
+            '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first().replace('\u00a3', '')
+
+        yield {
+            'Title': title,
+            'Image': final_image,
+            'Price': price,
+            'Stock': stock,
+            'Stars': stars,
+            'Description': description,
+            'Upc': upc,
+            'Price after tax': price_excl_tax,
+            'Price incl tax': price_inc_tax,
+            'Tax': tax,
+            'Number of reviews': number_of_reviews,
+        }
